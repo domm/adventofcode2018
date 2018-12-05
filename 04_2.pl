cd967d1cc24ba1dce128b7521bea4e81213f0ee0
Thomas Klausner <domm@plix.at>
2018-12-05 20:20:04 +0100

    less debug, nicer output

--- 04_2.pl
+++ 04_2.pl
@@ -9,7 +9,7 @@ my $start;
 my %stats;
 my $highest=0;
 my $highest_guard;
-my $per_min_max;
+my $per_min_max=0;
 my $per_min_min;
 my $per_min_guard;
 foreach my $l (@rec) {
@@ -23,8 +23,6 @@ foreach my $l (@rec) {
     }
     if ($l =~ /wakes/) {
         my $end = $min -1;
-        say "$l sleeps from $start to $end";
-        #say $stats{$current_guard}{total}++;
         for ($start .. $end) {
             $stats{$current_guard}{total}++;
             $stats{$current_guard}{$_}++;
@@ -34,22 +32,12 @@ foreach my $l (@rec) {
                 $per_min_guard = $current_guard;
             }
         }
-    #$say $stats{$current_guard}{total}++;
         if ($stats{$current_guard}{total} > $highest) {
             $highest_guard=$current_guard;
             $highest = $stats{$current_guard}{total};
         }
     }
 }
-
-
-foreach (reverse sort map { $_->{total} } values %stats) {
-    say $_;
-}
-
-use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \%stats;
-
-use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper $stats{$highest_guard};
 
 my $hit=0;
 my $max=undef;
@@ -61,6 +49,6 @@ while (my ($min,$count) = each %{$stats{$highest_guard}}) {
     }
 }
 
-say "$highest_guard -> $max ".$highest_guard * $max;
+say "1: $highest_guard -> $max; answer: ".$highest_guard * $max;
 
-say "2: $per_min_guard -> $per_min_min ($per_min_max) ".$per_min_guard * $per_min_min;
+say "2: $per_min_guard -> $per_min_min ($per_min_max) answer: ".$per_min_guard * $per_min_min;
