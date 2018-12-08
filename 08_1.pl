bea2ac0475955136b27290f64a772270fe59020a
Thomas Klausner <domm@plix.at>
2018-12-08 09:26:44 +0100

    day 08 part 1

---
+++ 08_1.pl
@@ -0,0 +1,23 @@
+use 5.020;
+use strict;
+use warnings;
+
+my @s = split(' ',<>);
+my $sum;
+while (@s) {
+    parse()
+}
+
+sub parse {
+    my $children = shift(@s);
+    my $meta =shift(@s);
+    exit unless defined $children;
+    for (1 .. $children) {
+        parse();
+    }
+    for (1 .. $meta) {
+        $sum+= shift(@s);
+    }
+}
+
+say $sum;
