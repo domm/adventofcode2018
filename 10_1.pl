cc5ca30613c96bae5180032f64d8fbf9c3437057
Thomas Klausner <domm@plix.at>
2018-12-10 13:13:30 +0100

    clean up

--- 10_1.pl
+++ 10_1.pl
@@ -12,37 +12,36 @@ while (<>) {
 }
 
 while (1) {
-    # move
-    foreach (@sky) {
-        $_->[0]+=$_->[2];
-        $_->[1]+=$_->[3];
-    }
-
-    # check
     my @map;
     my ($max, $min)=(0,0);
     foreach (@sky) {
+        # move
+        $_->[0]+=$_->[2];
+        $_->[1]+=$_->[3];
+
+        # get min/max x so we can calc vertical size
         $max = $_->[0] if $_->[0] > $max;
         $min = $_->[0] if $_->[0] < $min;
     }
+
+    # calc vertical size
     my $size = $max - $min;
     if ($size < $smallest) {
         $smallest = $prev = $size;
     }
 
-    # found it!
-    if ($size > $prev) {
-
-        # revert time!
-        foreach (@sky) {
-            $_->[0]-=$_->[2];
-            $_->[1]-=$_->[3];
-        }
-
+    if ($size > $prev) { # found it!
         my @word;
         my @box=(1000,0,1000,0);
         foreach (@sky) {
+            # travel back in time!
+            $_->[0]-=$_->[2];
+            $_->[1]-=$_->[3];
+
+            # draw the point
             $word[$_->[0]][$_->[1]]="#";
+
+            # figure out the exact bounding box
             $box[0] = $_->[0] if $_->[0] < $box[0];
             $box[1] = $_->[0] if $_->[0] > $box[1];
             $box[2] = $_->[1] if $_->[1] < $box[2];
